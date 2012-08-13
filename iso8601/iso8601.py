55b532be18b4bbac23da9a0a4af2b52e46febdfe
Peter Scott <peter@greplin.com>
2012-08-13 13:58:55 -0700

    Get rid of some redundant code.

--- iso8601/iso8601.py
+++ iso8601/iso8601.py
@@ -25,23 +25,9 @@ class ParseError(Exception):
 
 # Yoinked from python docs
 ZERO = timedelta(0)
-class Utc(tzinfo):
-    """UTC
-    
-    """
-    def utcoffset(self, dt):
-        return ZERO
-
-    def tzname(self, dt):
-        return "UTC"
-
-    def dst(self, dt):
-        return ZERO
-UTC = Utc()
-
 class FixedOffset(tzinfo):
     """Fixed offset in hours and minutes from UTC
-    
+
     """
     def __init__(self, offset_hours, offset_minutes, name):
         self.__offset = timedelta(hours=offset_hours, minutes=offset_minutes)
@@ -55,13 +41,14 @@ class FixedOffset(tzinfo):
 
     def dst(self, dt):
         return ZERO
-    
+
     def __repr__(self):
         return "<FixedOffset %r>" % self.__name
+UTC = FixedOffset(0, 0, "UTC")
 
 def parse_timezone(tzstring, default_timezone=UTC):
     """Parses ISO 8601 time zone specs into tzinfo offsets
-    
+
     """
     if tzstring == "Z":
         return default_timezone
@@ -80,7 +67,7 @@ def parse_timezone(tzstring, default_timezone=UTC):
 
 def parse_date(datestring, default_timezone=UTC):
     """Parses ISO 8601 dates into datetime objects
-    
+
     The timezone is parsed from the date string. However it is quite common to
     have dates without a timezone (not strictly correct). In this case the
     default timezone specified in default_timezone is used. This is UTC by
